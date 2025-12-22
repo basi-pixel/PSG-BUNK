2ff94ac3b08115c987b1880de887d873c8f29611
basi-pixel <ziab083@gmail.com>
2025-12-22 21:58:27 +0530

    Update sitemap.py

--- api/sitemap.py
+++ api/sitemap.py
@@ -1,11 +1,17 @@
-from flask import Response
+from http.server import BaseHTTPRequestHandler
 
-def handler(request):
-    xml = """<?xml version="1.0" encoding="UTF-8"?>
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "application/xml")
+        self.end_headers()
+
+        xml = """<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://psg-bunk.vercel.app/</loc>
     <priority>1.0</priority>
   </url>
-</urlset>"""
-    return Response(xml, mimetype="application/xml")
+</urlset>
+"""
+        self.wfile.write(xml.encode("utf-8"))
